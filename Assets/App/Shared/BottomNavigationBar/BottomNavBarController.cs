9b802dd48d739b26b9b0be16096fc496974b549b
Nick Matthews <nickd.mf7@gmail.com>
2024-07-16 13:21:17 -0500

    forgot to push changes

--- Assets/App/Shared/BottomNavigationBar/BottomNavBarController.cs
+++ Assets/App/Shared/BottomNavigationBar/BottomNavBarController.cs
@@ -11,6 +11,8 @@ namespace App.Shared.BottomNavigationBar
         public Button communityPageIconButton;
         public Button mapPageIconButton;
         public Button myProfileIconButton;
+
+        private int _currentPageIndex;
         
         void Start()
         {
@@ -18,26 +20,52 @@ namespace App.Shared.BottomNavigationBar
             communityPageIconButton.onClick.AddListener(_onCommunityPageClick);
             mapPageIconButton.onClick.AddListener(_onMapPageClick);
             myProfileIconButton.onClick.AddListener(_onMyProfileClick);
+            
+            // get the current scene name
+            var currentSceneName = SceneManager.GetActiveScene().name;
+            switch (currentSceneName)
+            {
+                case "HomePage":
+                    _currentPageIndex = 0;
+                    break;
+                case "CommunityPage":
+                    _currentPageIndex = 1;
+                    break;
+                case "MapPage":
+                    _currentPageIndex = 2;
+                    break;
+                case "ProfilePage":
+                    _currentPageIndex = 3;
+                    break;
+            }
         }
     
         void _onHomePageClick()
         {
+            if (_currentPageIndex == 0) return;
             SceneManager.LoadScene("HomePage");
+            _currentPageIndex = 0;
         }
     
         void _onCommunityPageClick()
         {
+            if (_currentPageIndex == 1) return;
             SceneManager.LoadScene("CommunityPage");
+            _currentPageIndex = 1;
         }
     
         void _onMapPageClick()
         {
+            if (_currentPageIndex == 2) return;
             SceneManager.LoadScene("MapPage");
+            _currentPageIndex = 2;
         }
     
         void _onMyProfileClick()
         {
+            if (_currentPageIndex == 3) return;
             SceneManager.LoadScene("ProfilePage");
+            _currentPageIndex = 3;
         }
     }
 }
